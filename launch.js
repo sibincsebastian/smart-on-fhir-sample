cc1d55665af63bd43b2b77fda0b957e78ae6f016
sibincsebastian <48143285+sibincsebastian@users.noreply.github.com>
2024-02-15 10:47:04 +0530

    Update launch.js

--- launch.js
+++ launch.js
@@ -3,8 +3,8 @@ function getFhirData() {
     console.log(clientId);
     FHIR.oauth2.authorize({
       'client_id': clientId,
-      'scope': 'user/Patient.read user/Practitioner.read launch openid profile offline_access fhirUser',
-      'redirect_uri': 'https://sibincsebastian.github.io/smart-on-fhir-sample/app.html'
+      'scope': 'user/Patient.read user/Obervation.read launch openid profile offline_access fhirUser',
+      'redirect_uri': 'https://sibincsebastian.github.io/smart-on-fhir-sample'
     });
   }
   
